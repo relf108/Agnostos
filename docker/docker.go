b4f75fb911a6d3b77e1a23001c4ffc0bb77ac0fb
Tristan Sutton <tristan.sutton@gmail.com>
2024-04-11 11:41:52 +1000

    StopDaemon() stub + generated code

--- docker/docker.go
+++ docker/docker.go
@@ -35,6 +35,28 @@ func StartDaemon() (out string, error string) {
 	return "Docker daemon started", ""
 }
 
+func StopDaemon() (out string, error string) {
+	// TODO @suttont: Test on all platforms - generated code
+	var cmdArr []string
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		cmdArr = []string{"osascript", "-e", `quit app "Docker"`}
+	case "windows":
+		cmdArr = []string{"stop-service", "docker"}
+	}
+	i := 0
+	cmdFirst := cmdArr[i]
+	cmdArr = append(cmdArr[:i], cmdArr[i+1:]...)
+	cmd := exec.Command(cmdFirst, cmdArr...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		return "", err.Error()
+	}
+	return "Docker daemon stopped", ""
+}
+
 func Run() {
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
